1ea2b923fa60b61ea731ec18168847194ee3ce83
CrazyRU <52334420+CrazyRU@users.noreply.github.com>
2019-06-29 00:35:47 +0300

    Add files via upload

---
+++ 3_hw.py
@@ -0,0 +1,37 @@
+print('this code helps you to find x in this equality')
+print('\nchoose 1 or 2: \n1)ax + b = 0 \n2)ax - b = 0')
+f = input()
+if f == '1':
+    print('enter a')
+    a = int(input())
+    print('enter non-negative b')
+    b = abs(int(input()))
+    if abs(a) > 1000 and abs(b) > 1000:
+        print("error, absolute numbers can't be more than 1000")
+    elif a == 1:
+        print('-------')
+        print(0 - b)
+    elif a == 0:
+        print('-------')
+        print('NO')
+    else:
+        print('-------')
+        print((0 - b) / a)
+elif f == '2':
+    print('enter a')
+    a = int(input())
+    print('enter non-negative b')
+    b = abs(int(input()))
+    if abs(a) > 1000 and abs(b) > 1000:
+        print("error, absolute numbers can't be more than 1000")
+    elif a == 1:
+        print('-------')
+        print(0 + b)
+    elif a == 0:
+        print('-------')
+        print('NO')
+    else:
+        print('-------')
+        print((0 + b) / a)
+else:
+    print('enter 1 or 2')
