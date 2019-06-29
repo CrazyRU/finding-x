9caf4a9fce499fa1411210f832ecbc56d32bb11c
CrazyRU <52334420+CrazyRU@users.noreply.github.com>
2019-06-29 13:07:15 +0300

    Update 3_hw.py

--- 3_hw.py
+++ 3_hw.py
@@ -1,4 +1,4 @@
-print('this code helps you to find x in this equality')
+#this code helps you to find x in this equality
 print('\nchoose 1 or 2: \n1)ax + b = 0 \n2)ax - b = 0')
 f = input()
 if f == '1':
@@ -6,7 +6,7 @@ if f == '1':
     a = int(input())
     print('enter non-negative b')
     b = abs(int(input()))
-    if abs(a) > 1000 and abs(b) > 1000:
+    if abs(a) > 1000 or abs(b) > 1000:
         print("error, absolute numbers can't be more than 1000")
     elif a == 1:
         print('-------')
@@ -22,7 +22,7 @@ elif f == '2':
     a = int(input())
     print('enter non-negative b')
     b = abs(int(input()))
-    if abs(a) > 1000 and abs(b) > 1000:
+    if abs(a) > 1000 or abs(b) > 1000:
         print("error, absolute numbers can't be more than 1000")
     elif a == 1:
         print('-------')
@@ -34,4 +34,4 @@ elif f == '2':
         print('-------')
         print((0 + b) / a)
 else:
-    print('enter 1 or 2')
+    print('enter 1 or 2') 
